d0a3e238b34f6c7fb744919b04d6782afb53a1ee
Sebastian <seb0465@hotmail.com>
2025-09-28 06:30:04 +0200

    Initial Schema and migration

---
+++ Student Management/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Management.Models
+{
+    public class Course
+    {
+        public required int Id { get; set; }
+        public required string Title { get; set; } = "";
+        public int Credits { get; set; } = 0;
+    }
+}
---
+++ Student Management/Models/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Management.Models
+{
+    public class Enrollment
+    {
+        public required int Id { get; set; }
+        public required int StudentId { get; set; }
+        public required int CourseId { get; set; }
+        public double Grade { get; set; }
+
+    }
+}
